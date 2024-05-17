0abd974a591c7ea79751fd55469ee34fcf6349ef
mona <heritageolaiy@gmail.com>
2024-05-17 17:10:33 +0100

    trying to use tailwind and shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+import './globals.css'
+
+
+
 export default function Page() {
 
   return(
-    <h1 className="bg-zinc-950 dark:bg-white text-blue-800 text-3xl">Hello, Next.js!</h1>
+    <h1 className="bg-zinc-950 dark:bg-white text-blue-800 text-xl">Hello, Heritage is here for Next.js!</h1>
   )
 }
