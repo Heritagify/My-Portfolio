96e7a5c85e0bf9385ef48037cb9ebbfedea325ae
mona <heritageolaiy@gmail.com>
2024-05-19 14:59:11 +0100

    creating components and trying to use AOS

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,23 +1,21 @@
-import '../app/globals.css'
-
-
-
-
+import "../app/globals.css";
 
 export default function Navbar() {
-    return (
-        <div className='flex justify-between mx-4 p-2 items-center'>
-            <h1 className='italic text-cyan-900 font-semibold text-2xl'>HERITAGIFY</h1>
-            <div className='flex space-x-5 text-sm '>
-                <h1>Home</h1>
-                <h1>About</h1>
-                <h1>Contact Us</h1>
-                <h1>Blog</h1>
-            </div>
-            <div>
-                <button className='bg-green-800 text-cyan-200 p-1'>Sign In</button>
-                <button>Sign Up</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="dark:bg-cyan-400 flex justify-between mx-4 p-2 items-center shadow-sm shadow-cyan-900">
+      <h1 className="italic text-cyan-900 font-semibold text-2xl">
+        HERITAGIFY
+      </h1>
+      <div className="hidden md:flex space-x-5 text-sm ">
+        <h1>Home</h1>
+        <h1>About</h1>
+        <h1>Contact Us</h1>
+        <h1>Blog</h1>
+      </div>
+      <div className="space-x-2 text-sm">
+        <button className="bg-green-800 text-cyan-200 p-1">Sign In</button>
+        <button>Sign Up</button>
+      </div>
+    </div>
+  );
 }
