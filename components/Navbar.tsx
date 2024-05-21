f4ca6be7579f5cdd53d8cbb000a1f994676625b6
mona <heritageolaiy@gmail.com>
2024-05-21 21:32:09 +0100

    more improvements with nextjs

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,28 +13,23 @@ import { FaInfoCircle } from "react-icons/fa";
 export default function Navbar() {
   return (
     <div>
-      <header className="flex justify-between mx-4 p-2 items-center shadow-sm shadow-cyan-900">
+      <header className="flex justify-between mx-24 p-2 items-center  container-fluid py-3">
         <h1 className="italic text-cyan-900 font-semibold text-2xl">
           HERITAGIFY
         </h1>
         <div className="hidden md:flex space-x-5 text-sm ">
-          <li className="flex items-center gap-1"><FaHome />Home</li>
-          <li className="flex items-center gap-1"><FaInfoCircle />About</li>
-          <li className="flex items-center gap-1"><GrTechnology />Tech Stacks</li>
-          <li className="flex items-center gap-1"><FaProjectDiagram />Projects</li>
-          <li className="flex items-center gap-1"><IoMdContact />Contact Me</li>
+          <li className="flex items-center gap-1 cursor-pointer"><FaHome />Home</li>
+          <li className="flex items-center gap-1 cursor-pointer"><FaInfoCircle />About</li>
+          <li className="flex items-center gap-1 cursor-pointer"><GrTechnology />Tech Stacks</li>
+          <li className="flex items-center gap-1 cursor-pointer"><FaProjectDiagram />Projects</li>
+          <li className="flex items-center gap-1 cursor-pointer"><IoMdContact />Contact Me</li>
         </div>
-        <div className="">
+        <div className="flex space-x-3">
           <FaGithub />
           <FaTwitter />
           <FaLinkedin />
         </div>
 
-
-        <div className="space-x-2 text-sm">
-          <button className="bg-green-800 text-cyan-200 p-1">Sign In</button>
-          <button>Sign Up</button>
-        </div>
       </header>
     </div>
   );
