03d09d7e64a0816622105dd0b53d3293d1d3dc97
mona <heritageolaiy@gmail.com>
2024-05-21 21:27:49 +0100

    more improvements with nextjs

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,14 @@
 import "../app/globals.css";
+import { FaGithub } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { IoMdContact } from "react-icons/io";
+import { FaProjectDiagram } from "react-icons/fa";
+import { GrTechnology } from "react-icons/gr";
+import { FaHome } from "react-icons/fa";
+import { FaInfoCircle } from "react-icons/fa";
+
+
 
 export default function Navbar() {
   return (
@@ -8,12 +18,19 @@ export default function Navbar() {
           HERITAGIFY
         </h1>
         <div className="hidden md:flex space-x-5 text-sm ">
-          <li>Home</li>
-          <li>About</li>
-          <li>Tech Stacks</li>
-          <li>Projects</li>
-          <li>Contact Me</li>
+          <li className="flex items-center gap-1"><FaHome />Home</li>
+          <li className="flex items-center gap-1"><FaInfoCircle />About</li>
+          <li className="flex items-center gap-1"><GrTechnology />Tech Stacks</li>
+          <li className="flex items-center gap-1"><FaProjectDiagram />Projects</li>
+          <li className="flex items-center gap-1"><IoMdContact />Contact Me</li>
         </div>
+        <div className="">
+          <FaGithub />
+          <FaTwitter />
+          <FaLinkedin />
+        </div>
+
+
         <div className="space-x-2 text-sm">
           <button className="bg-green-800 text-cyan-200 p-1">Sign In</button>
           <button>Sign Up</button>
