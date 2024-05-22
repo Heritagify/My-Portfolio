0a17af21ad11e4d3fed437e631303422414d4f4e
mona <heritageolaiy@gmail.com>
2024-05-22 14:57:22 +0100

    portfolio in progress

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,26 @@
 import './globals.css'
 import { useAos } from "./utils/useAos";
 import Navbar from '.././components/Navbar'
+import { TfiShine } from "react-icons/tfi";
+import { MdDarkMode } from "react-icons/md";
+import { VscVscode } from "react-icons/vsc"; <VscVscode />
+import { FaGithub } from "react-icons/fa"; <FaGithub />
+import { FaHtml5 } from "react-icons/fa6"; <FaHtml5 />
+import { FaCss3Alt } from "react-icons/fa"; <FaCss3Alt />
+import { IoLogoJavascript } from "react-icons/io"; <IoLogoJavascript />
+import { SiTailwindcss } from "react-icons/si"; <SiTailwindcss />
+import { FaReact } from "react-icons/fa"; <FaReact />
+import { SiVite } from "react-icons/si"; <SiVite />
+import { RiNextjsFill } from "react-icons/ri"; <RiNextjsFill />
+import { SiMongodb } from "react-icons/si"; <SiMongodb />
+import { IoLogoVercel } from "react-icons/io5"; <IoLogoVercel />
+import PortPic from './'
+
+
+
+
+
+
 
 
 export default function Page() {
@@ -10,28 +30,19 @@ export default function Page() {
   return (
     <div className="dark">
       <Navbar/>
-      <h1
-        data-aos="fade-right"
-        className="text-4xl font-bold mb-8">My Portfolio</h1>
-      <div data-aos="fade-up" className="mb-8">
-        <h2 className="text-2xl font-semibold">About</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dapibus
-          sapien ac nisi bibendum, vel malesuada nisi tincidunt.
-        </p>
+      <div className="mx-28 mt-28">
+        <h1
+          data-aos="fade-right"
+          className="text-4xl font-bold mb-8">HI!</h1>
+          <h1>I'm OLAIYA HERITAGE .J</h1>
+          <h1>Frontend Developer</h1>
+          <p></p>
+          <div className="flex gap-4">
+            <button className="cursor-pointer p-2 px-3 rounded-lg bg-white text-sm text-teal-600 font-semibold hover:text-emerald-200 shadow-md shadow-teal-600">Download CV</button>
+            <button className="cursor-pointer p-2 px-3 text-sm text-teal-200 rounded-lg hover:text-emerald-200 shadow-md shadow-teal-600">Hire Me</button>
+          </div>
       </div>
-      <div data-aos="fade-left" className="container mb-8 border-2 border-red-600">
-        <h2 className="text-2xl font-semibold">Projects</h2>
-        <ul>
-          <li>Project 1</li>
-          <li>Project 2</li>
-          <li>Project 3</li>
-        </ul>
-      </div>
-      <div data-aos="fade-right">
-        <h2 className="text-2xl font-semibold">Contact</h2>
-        <p>Get in touch with me at example@example.com</p>
-      </div>
+     
     </div>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,40 +7,50 @@ import { FaProjectDiagram } from "react-icons/fa";
 import { GrTechnology } from "react-icons/gr";
 import { FaHome } from "react-icons/fa";
 import { FaInfoCircle } from "react-icons/fa";
+import { IoMenu } from "react-icons/io5"; <IoMenu />
+import { LiaTimesSolid } from "react-icons/lia"; <LiaTimesSolid />
+
+
 
 export default function Navbar() {
   return (
     <div>
-      <header className="flex justify-between mx-24 p-2 items-center  container-fluid py-3">
-        <h1 className="italic text-cyan-900 font-semibold text-2xl">
+      <header className="flex justify-between mx-44 p-2 items-center  container-fluid py-3">
+        <h1 className="italic text-cyan-300 font-semibold text-2xl">
           HERITAGIFY
         </h1>
-        <div className="hidden md:flex space-x-9 text-xs font-semibold">
-          <li className="flex items-center gap-1 cursor-pointer">
+        <div className="hidden md:flex space-x-4 text-xs font-semibold">
+          <li className="flex items-center gap-1 cursor-pointer p-2 rounded-lg hover:text-emerald-200 shadow-md shadow-teal-600">
             <FaHome />
             Home
           </li>
-          <li className="flex items-center gap-1 cursor-pointer">
+          <li className="flex items-center gap-1 cursor-pointer p-2 rounded-lg hover:text-emerald-200 hover:shadow-sm hover:shadow-teal-500">
             <FaInfoCircle />
             About
           </li>
-          <li className="flex items-center gap-1 cursor-pointer">
+          <li className="flex items-center gap-1 cursor-pointer p-2 rounded-lg hover:text-emerald-200 hover:shadow-sm hover:shadow-teal-500">
             <GrTechnology />
             Tech Stacks
           </li>
-          <li className="flex items-center gap-1 cursor-pointer">
+          <li className="flex items-center gap-1 cursor-pointer p-2 rounded-lg hover:text-emerald-200 hover:shadow-sm hover:shadow-teal-500">
             <FaProjectDiagram />
             Projects
           </li>
-          <li className="flex items-center gap-1 cursor-pointer">
+          <li className="flex items-center gap-1 cursor-pointer p-2 rounded-lg hover:text-emerald-200 hover:shadow-md hover:shadow-teal-500">
             <IoMdContact />
             Contact Me
           </li>
         </div>
-        <div className="flex space-x-3">
-          <FaGithub />
-          <FaTwitter />
-          <FaLinkedin />
+        <div className="flex space-x-1">
+          <button  className="cursor-pointer p-2 rounded-lg text-teal-200">
+            <FaGithub />
+          </button>
+          <button  className="cursor-pointer p-2 rounded-lg text-teal-200">
+            <FaTwitter />
+          </button>
+          <button  className="cursor-pointer p-2 rounded-lg text-teal-200">
+            <FaLinkedin />
+          </button>
         </div>
       </header>
     </div>
