ec5947c95fedb03ffa762db2c38ef6db753490c1
mona <heritageolaiy@gmail.com>
2024-05-23 20:21:12 +0100

    working on the tech stacks

--- app/page.tsx
+++ app/page.tsx
@@ -11,28 +11,18 @@ import { FaLocationDot } from "react-icons/fa6";
 import { IoIosCall } from "react-icons/io";
 import { IoIosMail } from "react-icons/io";
 import { VscVscode } from "react-icons/vsc";
-<VscVscode />;
 import { FaGithub } from "react-icons/fa";
-<FaGithub />;
+import { FaGitAlt } from "react-icons/fa";
 import { FaHtml5 } from "react-icons/fa6";
 import { FaCss3Alt } from "react-icons/fa";
-<FaCss3Alt />;
 import { IoLogoJavascript } from "react-icons/io";
-<IoLogoJavascript />;
 import { SiTailwindcss } from "react-icons/si";
-<SiTailwindcss />;
 import { FaReact } from "react-icons/fa";
-<FaReact />;
 import { SiVite } from "react-icons/si";
-<SiVite />;
 import { RiNextjsFill } from "react-icons/ri";
-<RiNextjsFill />;
 import { SiTypescript } from "react-icons/si";
-<SiTypescript />;
 import { SiMongodb } from "react-icons/si";
-<SiMongodb />;
 import { IoLogoVercel } from "react-icons/io5";
-<IoLogoVercel />;
 import PortPic from ".././public/portPic.jpg";
 import AboutMe from ".././public/aboutMe.webp";
 
@@ -140,10 +130,94 @@ export default function Page() {
         </div>
       </div>
       {/* --------TECH STACKS------ */}
-      <center>TECH STACKS</center>
-      <div>
-        <div className="backdrop-blur-md bg-white/30 rounded-full w-fit p-4">
-          <FaHtml5 className="text-3xl"/>
+      <div className="py-20 space-y-5">
+        <center className="text-2xl font-semibold">TECH STACKS</center>
+        <div className="space-y-4">
+          <div className="flex justify-center gap-8">
+            <div className="group relative flex items-center justify-center backdrop-blur-lg bg-white/30 rounded-full w-fit p-4">
+              <FaHtml5 className="text-4xl text-red-400" />
+              <span className="absolute -top-12 left-1/2 -translate-x-1/2 px-3 py-2 bg-gradient-to-r from-red-400 to-red-500 text-white text-sm rounded-full shadow-md opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                HTML
+              </span>
+            </div>
+            <div className="group relative flex items-center justify-center backdrop-blur-lg bg-white/30 rounded-full w-fit p-4">
+              <FaCss3Alt className="text-4xl text-red-400" />
+              <span className="absolute -top-12 left-1/2 -translate-x-1/2 px-3 py-2 bg-gradient-to-r from-red-400 to-red-500 text-white text-sm rounded-full shadow-md opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                CSS
+              </span>
+            </div>
+
+            <div className="group relative flex items-center justify-center backdrop-blur-lg bg-white/30 rounded-full w-fit p-4">
+              <IoLogoJavascript className="text-4xl text-red-400" />
+              <span className="absolute -top-12 left-1/2 -translate-x-1/2 px-3 py-2 bg-gradient-to-r from-red-400 to-red-500 text-white text-sm rounded-full shadow-md opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                JAVASCRIPT
+              </span>
+            </div>
+            <div className="group relative flex items-center justify-center backdrop-blur-lg bg-white/30 rounded-full w-fit p-4">
+              <FaReact className="text-4xl text-red-400" />
+              <span className="absolute -top-12 left-1/2 -translate-x-1/2 px-3 py-2 bg-gradient-to-r from-red-400 to-red-500 text-white text-sm rounded-full shadow-md opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                REACTJS
+              </span>
+            </div>
+            <div className="group relative flex items-center justify-center backdrop-blur-lg bg-white/30 rounded-full w-fit p-4">
+              <SiVite className="text-3xl text-orange-600" />
+              <span className="absolute -top-12 left-1/2 -translate-x-1/2 px-3 py-2 bg-gradient-to-r from-red-400 to-red-500 text-white text-sm rounded-full shadow-md opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                VITE
+              </span>
+            </div>
+            <div className="group relative flex items-center justify-center backdrop-blur-lg bg-white/30 rounded-full w-fit p-4">
+              <SiTailwindcss className="text-3xl text-blue-600 animate-pulse" />
+              <span className="absolute -top-12 left-1/2 -translate-x-1/2 px-3 py-2 bg-gradient-to-r from-red-400 to-red-500 text-white text-sm rounded-full shadow-md opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                TAILWIND CSS
+              </span>
+            </div>
+            <div className="group relative flex items-center justify-center backdrop-blur-lg bg-white/30 rounded-full w-fit p-4">
+              <RiNextjsFill className="text-3xl text-black" />
+              <span className="absolute -top-12 left-1/2 -translate-x-1/2 px-3 py-2 bg-gradient-to-r from-red-400 to-red-500 text-white text-sm rounded-full shadow-md opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                NEXT JS
+              </span>
+            </div>
+            <div className="group relative flex items-center justify-center backdrop-blur-lg bg-white/30 rounded-full w-fit p-4">
+              <SiTypescript className="text-3xl text-cyan-700" />
+              <span className="absolute -top-12 left-1/2 -translate-x-1/2 px-3 py-2 bg-gradient-to-r from-red-400 to-red-500 text-white text-sm rounded-full shadow-md opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                TYPESCRIPT
+              </span>
+            </div>
+          </div>
+          <div className="flex justify-center gap-6">
+            <div className="group relative flex items-center justify-center backdrop-blur-lg bg-white/30 rounded-full w-fit p-4">
+              <FaGithub className="text-3xl text-blue-400" />
+              <span className="absolute -top-12 left-1/2 -translate-x-1/2 px-3 py-2 bg-gradient-to-r from-red-400 to-red-500 text-white text-sm rounded-full shadow-md opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                GITHUB
+              </span>
+            </div>
+            <div className="group relative flex items-center justify-center backdrop-blur-lg bg-white/30 rounded-full w-fit p-4">
+              <SiMongodb className="text-3xl text-green-500 animate-bounce" />
+              <span className="absolute -top-12 left-1/2 -translate-x-1/2 px-3 py-2 bg-gradient-to-r from-red-400 to-red-500 text-white text-sm rounded-full shadow-md opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                MONGODB
+              </span>
+            </div>
+            <div className="group relative flex items-center justify-center backdrop-blur-lg bg-white/30 rounded-full w-fit p-4">
+              <IoLogoVercel className="text-3xl text-green-500" />
+              <span className="absolute -top-12 left-1/2 -translate-x-1/2 px-3 py-2 bg-gradient-to-r from-red-400 to-red-500 text-white text-sm rounded-full shadow-md opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                VERCEL
+              </span>
+            </div>
+            <div className="group relative flex items-center justify-center backdrop-blur-lg bg-white/30 rounded-full w-fit p-4">
+              <VscVscode className="text-3xl text-cyan-400" />
+              <span className="absolute -top-12 left-1/2 -translate-x-1/2 px-3 py-2 bg-gradient-to-r from-red-400 to-red-500 text-white text-sm rounded-full shadow-md opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                VS-CODE
+              </span>
+            </div>
+            <div className="group relative flex items-center justify-center backdrop-blur-lg bg-white/30 rounded-full w-fit p-4">
+              <FaGitAlt className="text-3xl text-cyan-400" />
+              <span className="absolute -top-12 left-1/2 -translate-x-1/2 px-3 py-2 bg-gradient-to-r from-red-400 to-red-500 text-white text-sm rounded-full shadow-md opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                GIT
+              </span>
+            </div>
+
+            <div className="backdrop-blur-md bg-white/30 rounded-full w-fit p-4"></div>
+          </div>
         </div>
       </div>
     </div>
