fc3bab0fe0a23ebcf66c2c4c2468bcfac30314c6
mona <heritageolaiy@gmail.com>
2024-05-27 19:47:02 +0100

    trying somethings out

--- app/page.tsx
+++ app/page.tsx
@@ -24,11 +24,14 @@ export default function Page() {
   useAos();
 
     return (
-      // <div className="backdrop-filter backdrop-blur-lg bg-gray-200">
-      // <div className="dark">
-      <div className="relative min-h-screen bg-gradient-to-r from-emerald-400 from-20% via-sky-500 via-30% to-emerald-500 to-90%">
-      <div className="absolute inset-0 bg-black bg-opacity-75 backdrop-blur-sm"></div>
-      <div className="relative z-10">
+      <div className="dark backdrop-filter backdrop-blur-sm bg-gray-900">
+        <div className="absolute bg-pink-500 h-96 w-1/3 rounded-full opacity-20 blur-3xl left-44 top-1/2" />
+        <div className="absolute bg-pink-400 h-96 w-1/3 rounded-full opacity-15 blur-3xl left-1/2 top-60" />
+        <div className="absolute bg-pink-600 h-96 w-1/3 rounded-full opacity-20 blur-3xl left-7 top-36" />
+      <div className="">
+      {/* // <div className="relative min-h-screen bg-gradient-to-r from-emerald-400 from-20% via-sky-500 via-30% to-emerald-500 to-90%">
+      // <div className="absolute inset-0 bg-black bg-opacity-75 backdrop-blur-sm"></div>
+      // <div className="relative z-10"> */}
         <Navbar />
         {/* ------------HERO SECTION_--------- */}
         <div className="mx-56 mt-28">
