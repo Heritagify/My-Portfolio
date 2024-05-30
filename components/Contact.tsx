f3221bdc979acab887533f2407023cdd29cd3dfe
mona <heritageolaiy@gmail.com>
2024-05-30 21:37:59 +0100

    services component

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -7,29 +7,32 @@ export default function Contact() {
     return(
         <div>
             <center className="text-2xl font-semibold py-5">Contact</center>
-                <div className="rounded-lg flex backdrop-filter bg-white bg-opacity-25 mx-20 py-4">
+                <div className="rounded-lg flex backdrop-filter bg-white bg-opacity-25 mx-20 py-8">
                     <div className="flex flex-col items-center justify-center py-5 w-1/2"></div>
                     <form className="w-1/2 space-y-6 mx-5">
-                        <div>
-                            <p className="">User Name</p>
-                            <input className="bg-white rounded-xl w-full p-2 py-3"
+                        <div className="space-y-2">
+                            <label className="">User Name</label>
+                            <input className="bg-white rounded-xl w-full p-2 py-3 shadow appearance-none border text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                 placeholder="Enter your name"
+                                type="text"
                             />
                         </div>
-                        <div>
-                            <p className="">Email</p>
-                            <input className="bg-white rounded-xl w-full p-2 py-3"
+                        <div className="space-y-2">
+                            <label className="">Email</label>
+                            <input className="bg-white rounded-xl w-full p-2 py-3 shadow appearance-none border text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                 placeholder="Enter Your Email"
+                                type="text"
                             />
                         </div>
-                        <div>
-                            <p className="">Message</p>
-                            <input className="bg-white rounded-xl w-full p-2 py-12"
+                        <div className="space-y-2">
+                            <label className="">Message</label>
+                            <input className="bg-white rounded-xl w-full p-2 py-12 shadow appearance-none border text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                 placeholder="Enter Your Message"
+                                type="text"
                             />
                         </div>
                         <input
-                            className="p-2 px-6 rounded-xl text-purple-400 bg-white hover:shadow-sm hover:shadow-white"
+                            className="p-2 px-6 rounded-lg font-bold cursor-pointer text-purple-400 bg-white hover:shadow-sm hover:shadow-white focus:outline-none focus:shadow-outline"
                             type="submit"
                         />
                     </form>
