45c28c56dac1f548ea68f958c83bb4dbd402e357
mona <heritageolaiy@gmail.com>
2024-05-31 21:43:10 +0100

    making the navbar responsive

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,39 +3,31 @@ import { motion, AnimatePresence } from "framer-motion";
 import { FaGithub, FaTwitter, FaLinkedin, FaHome, FaInfoCircle, FaProjectDiagram } from "react-icons/fa";
 import { IoMdContact } from "react-icons/io";
 import { GrTechnology } from "react-icons/gr";
-import { IoMenu } from "react-icons/io5";
-import { LiaTimesSolid } from "react-icons/lia";
-import { useState } from 'react';
-
-
+import { IoMenu, IoClose } from "react-icons/io5";
+import React, { useState } from 'react';
 
 export default function Navbar() {
-
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   const toggleMobileMenu = () => {
     setMobileMenuOpen(!isMobileMenuOpen);
   };
 
-
-
   return (
     <div>
-      <header className="border-2 flex justify-between mx-36 p-8 items-center  container-fluid py-3">
-        <h1 className="italic text-cyan-300 font-semibold text-2xl">
+      <header className="border-2 flex justify-between mx-4 md:mx-36 p-4 md:p-8 items-center container-fluid py-3">
+        <h1 className="italic text-cyan-300 font-semibold text-xl md:text-2xl">
           HERITAGIFY
         </h1>
 
-        <div className="lg:hidden">
+        <div className="md:hidden">
           <button onClick={toggleMobileMenu} className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-            {isMobileMenuOpen ? <LiaTimesSolid className="h-6 w-6" /> : <IoMenu className="h-6 w-6" />}
+            {isMobileMenuOpen ? <IoClose className="h-6 w-6" /> : <IoMenu className="h-6 w-6" />}
           </button>
         </div>
 
-
-
-        <div className={`lg:flex space-x-4 text-md font-semibold ${isMobileMenuOpen ? 'block' : 'hidden'}`}>
-          <ul className="lg:flex lg:space-x-4 text-md font-semibold">
+        <nav className={`md:flex space-x-4 text-sm md:text-md font-semibold ${isMobileMenuOpen ? 'block' : 'hidden md:block'}`}>
+          <ul className="md:flex md:space-x-4">
             <li className="flex items-center gap-1 cursor-pointer p-2 rounded-lg hover:text-emerald-200 shadow-md shadow-teal-600">
               <FaHome />
               Home
@@ -57,50 +49,51 @@ export default function Navbar() {
               Contact Me
             </li>
           </ul>
-        </div>
+        </nav>
 
         <div className="flex space-x-1">
-          <button  className="cursor-pointer p-2 rounded-lg text-teal-200">
+          <button className="cursor-pointer p-2 rounded-lg text-teal-200">
             <FaGithub />
           </button>
-          <button  className="cursor-pointer p-2 rounded-lg text-teal-200">
+          <button className="cursor-pointer p-2 rounded-lg text-teal-200">
             <FaTwitter />
           </button>
-          <button  className="cursor-pointer p-2 rounded-lg text-teal-200">
+          <button className="cursor-pointer p-2 rounded-lg text-teal-200">
             <FaLinkedin />
           </button>
         </div>
       </header>
 
-
-       <AnimatePresence>
+      <AnimatePresence>
         {isMobileMenuOpen && (
           <motion.div
             initial={{ opacity: 0, height: 0 }}
             animate={{ opacity: 1, height: "auto" }}
             exit={{ opacity: 0, height: 0 }}
-            className="lg:hidden px-6 py-2 space-y-2"
+            className="px-6 py-2 space-y-2"
           >
-            <li className="flex items-center gap-1 cursor-pointer p-2 rounded-lg hover:text-emerald-200 shadow-md shadow-teal-600">
-              <FaHome />
-              Home
-            </li>
-            <li className="flex items-center gap-1 cursor-pointer p-2 rounded-lg hover:text-emerald-200 hover:shadow-sm hover:shadow-teal-500">
-              <FaInfoCircle />
-              About
-            </li>
-            <li className="flex items-center gap-1 cursor-pointer p-2 rounded-lg hover:text-emerald-200 hover:shadow-sm hover:shadow-teal-500">
-              <GrTechnology />
-              Tech Stacks
-            </li>
-            <li className="flex items-center gap-1 cursor-pointer p-2 rounded-lg hover:text-emerald-200 hover:shadow-sm hover:shadow-teal-500">
-              <FaProjectDiagram />
-              Projects
-            </li>
-            <li className="flex items-center gap-1 cursor-pointer p-2 rounded-lg hover:text-emerald-200 hover:shadow-md hover:shadow-teal-500">
-              <IoMdContact />
-              Contact Me
-            </li>
+            <ul>
+              <li className="flex items-center gap-1 cursor-pointer p-2 rounded-lg hover:text-emerald-200 shadow-md shadow-teal-600">
+                <FaHome />
+                Home
+              </li>
+              <li className="flex items-center gap-1 cursor-pointer p-2 rounded-lg hover:text-emerald-200 hover:shadow-sm hover:shadow-teal-500">
+                <FaInfoCircle />
+                About
+              </li>
+              <li className="flex items-center gap-1 cursor-pointer p-2 rounded-lg hover:text-emerald-200 hover:shadow-sm hover:shadow-teal-500">
+                <GrTechnology />
+                Tech Stacks
+              </li>
+              <li className="flex items-center gap-1 cursor-pointer p-2 rounded-lg hover:text-emerald-200 hover:shadow-sm hover:shadow-teal-500">
+                <FaProjectDiagram />
+                Projects
+              </li>
+              <li className="flex items-center gap-1 cursor-pointer p-2 rounded-lg hover:text-emerald-200 hover:shadow-md hover:shadow-teal-500">
+                <IoMdContact />
+                Contact Me
+              </li>
+            </ul>
           </motion.div>
         )}
       </AnimatePresence>
