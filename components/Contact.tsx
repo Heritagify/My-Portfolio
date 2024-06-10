e2d5b60f8270e2e62028f6070033a6c6e5f4af99
mona <heritageolaiy@gmail.com>
2024-06-10 20:03:45 +0100

    footer

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,34 +1,20 @@
-import { forwardRef } from 'react';
-import Image from "next/image";
-
-import ContactMe from ".././public/contact.svg";
-import { SiMinutemailer } from "react-icons/si";
-import "../app/globals.css";
-
-
-
-
+import React, { forwardRef } from 'react';
+import Image from 'next/image';
+import { SiMinutemailer } from 'react-icons/si';
+import ContactMe from '.././public/contact.svg';
+import '../app/globals.css';
 
 interface ContactProps {
   id: string;
 }
 
-
 const Contact = forwardRef<HTMLDivElement, ContactProps>(({ id }, ref) => {
-
-  // const contactRef = useRef(null);
-
-
   return (
     <div ref={ref} id={id}>
       <center className="text-3xl font-semibold py-9">Contact</center>
       <div className="rounded-lg flex backdrop-blur-lg bg-white/30 bg-opacity-25 mx-20 py-10">
         <div className="flex justify-center items-center w-1/2">
-          <Image
-            src={ContactMe}
-            alt="Contact me"
-            className="w-96 h-96"
-          />
+          <Image src={ContactMe} alt="Contact me" className="w-96 h-96" />
         </div>
         <div className="flex flex-col items-center justify-center py-5"></div>
         <form className="w-1/2 space-y-6 mx-5">
@@ -48,27 +34,26 @@ const Contact = forwardRef<HTMLDivElement, ContactProps>(({ id }, ref) => {
               type="text"
             />
           </div>
-          <label
-            // for="message"
-            className="block text-sm font-medium text-gray-900 dark:text-gray-400"
-          >
+          <label className="block text-sm font-medium text-gray-900 dark:text-gray-400">
             Your message
           </label>
           <textarea
-            rows="4"
+            rows={4}
             className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Enter your Message please....."
           ></textarea>
 
           <div className="flex justify-center">
-            <button className="flex justify-center items-center gap-1 p-2 px-6 rounded-lg font-semibold cursor-pointer dark:text-indigo-600 bg-indigo-700 text-gray-300 dark:bg-gray-300 hover:shadow-md hover:shadow-white focus:outline-none focus:shadow-outline"
-              >Send Message <SiMinutemailer className="text-2xl"/>
+            <button className="flex justify-center items-center gap-1 p-2 px-6 rounded-lg font-semibold cursor-pointer dark:text-indigo-600 bg-indigo-700 text-gray-300 dark:bg-gray-300 hover:shadow-md hover:shadow-white focus:outline-none focus:shadow-outline">
+              Send Message <SiMinutemailer className="text-2xl" />
             </button>
           </div>
-          
         </form>
       </div>
     </div>
   );
 });
+
+Contact.displayName = 'Contact';
+
 export default Contact;
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,30 @@
 import Image from "next/image";
 import "../app/globals.css";
-import Wave from ".././public/wave.svg";
+import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
+import { IoLogoWhatsapp } from "react-icons/io";
 
 export default function Footer() {
   return (
-    <div className="relative mt-40 rounded-lg backdrop-blur-lg bg-white/30 bg-opacity-25 mx-20 py-5 wave">
-      
+    <div className="text-teal-200 mt-40 rounded-lg backdrop-blur-lg bg-white/30 bg-opacity-25 mx-20 py-5">
+      <div className="flex justify-between mx-20">
+        <div className="border-4 space-y-3">
+          <p className="text-sm">My social media handles:</p>
+
+          <div className="flex space-x-9 text-lg text-emerald-400">
+            <FaTwitter />
+            <IoLogoWhatsapp />
+            <IoMdMail />
+            <FaLinkedin />
+          </div>
+        </div>
+
+        <div className="border-4">
+          <FaGithub />
+        </div>
+      </div>
+
+      <center className="">&#169;2023</center>
     </div>
   );
 }
