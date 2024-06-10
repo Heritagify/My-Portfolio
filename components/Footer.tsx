66a776842da8a7f0c8db7dae82fb5d8be4f6fd23
mona <heritageolaiy@gmail.com>
2024-06-10 20:53:35 +0100

    footer links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import Link from "next/link";
+
 import "../app/globals.css";
 import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
 import { IoMdMail } from "react-icons/io";
@@ -6,25 +8,37 @@ import { IoLogoWhatsapp } from "react-icons/io";
 
 export default function Footer() {
   return (
-    <div className="text-teal-200 mt-40 rounded-lg backdrop-blur-lg bg-white/30 bg-opacity-25 mx-20 py-5">
+    <div className="text-teal-200 mt-10 rounded-lg backdrop-blur-lg bg-white/30 bg-opacity-25 mx-20 py-10">
       <div className="flex justify-between mx-20">
-        <div className="border-4 space-y-3">
+        <div className="space-y-3">
           <p className="text-sm">My social media handles:</p>
 
-          <div className="flex space-x-9 text-lg text-emerald-400">
-            <FaTwitter />
-            <IoLogoWhatsapp />
-            <IoMdMail />
-            <FaLinkedin />
+          <div className="flex space-x-9 text-2xl text-emerald-400">
+            <Link href="https://twitter.com/olaiya_Heritage">
+              <FaTwitter className="cursor-pointer hover:text-red-300 hover:animate-bounce" />
+            </Link>
+            <Link href=" https://wa.me/+2348146041496">
+              <IoLogoWhatsapp className="cursor-pointer hover:text-red-300 hover:animate-bounce" />
+            </Link>
+            <Link href="">
+              <IoMdMail className="cursor-pointer hover:text-red-300 hover:animate-bounce" />
+            </Link>
+            <Link href="https://www.linkedin.com/in/olaiya-heritage">
+              <FaLinkedin className="cursor-pointer hover:text-red-300 hover:animate-bounce" />
+            </Link>
           </div>
         </div>
 
-        <div className="border-4">
+        <Link href="https://github.com/Heritagify" className="border-4">
           <FaGithub />
-        </div>
+        </Link>
       </div>
 
-      <center className="">&#169;2023</center>
+      <div className="flex justify-center items-center mt-3">
+        <div className="w-48 md:w-44 border-t border-teal-500"></div>
+        <div className="mx-2 md:text-sm text-teal-700-500">&#169; 2023</div>
+        <div className="w-48 md:w-44 border-t border-teal-500"></div>
+      </div>
     </div>
   );
 }
