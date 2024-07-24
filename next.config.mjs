01dfea1cf728c73a87a1384b176202f86df78bd0
mona <heritageolaiy@gmail.com>
2024-07-24 20:38:06 +0100

    still solving vercel bugs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  swcMinify: true,
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: "https",
         hostname: "**",
       },
     ],
